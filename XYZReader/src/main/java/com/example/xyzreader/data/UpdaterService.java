5acf70ab4b93d48878e1e0d77b786f3d2cde57b1
Joshua Irwin <jirwin.dev@gmail.com>
2016-06-01 23:17:21 -0400

    Added coordinator layout

--- XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
+++ XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
@@ -9,7 +9,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.RemoteException;
-import android.text.format.Time;
 import android.util.Log;
 
 import com.example.xyzreader.remote.RemoteEndpointUtil;
@@ -22,72 +21,73 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class UpdaterService extends IntentService {
-    private static final String TAG = "UpdaterService";
+	private static final String TAG = "UpdaterService";
 
-    public static final String BROADCAST_ACTION_STATE_CHANGE
-            = "com.example.xyzreader.intent.action.STATE_CHANGE";
-    public static final String EXTRA_REFRESHING
-            = "com.example.xyzreader.intent.extra.REFRESHING";
+	public static final String BROADCAST_ACTION_STATE_CHANGE
+			= "com.example.xyzreader.intent.action.STATE_CHANGE";
+	public static final String EXTRA_REFRESHING
+			= "com.example.xyzreader.intent.extra.REFRESHING";
 
-    public UpdaterService() {
-        super(TAG);
-    }
+	public UpdaterService() {
+		super(TAG);
+	}
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-//        Time time = new Time();
+	@Override
+	protected void onHandleIntent(Intent intent) {
+//		Time time = new Time();
 
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo ni = cm.getActiveNetworkInfo();
-        if (ni == null || !ni.isConnected()) {
-            Log.w(TAG, "Not online, not refreshing.");
-            return;
-        }
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni == null || !ni.isConnected()) {
+			Log.w(TAG, "Not online, not refreshing.");
+			return;
+		}
 
-        sendStickyBroadcast(
-                new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
+		sendStickyBroadcast(
+				new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
 
-        // Don't even inspect the intent, we only do one thing, and that's fetch content.
-        ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
+		// Don't even inspect the intent, we only do one thing, and that's fetch content.
+		ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
 
-        Uri dirUri = ItemsContract.Items.buildDirUri();
+		Uri dirUri = ItemsContract.Items.buildDirUri();
 
-        // Delete all items
-        cpo.add(ContentProviderOperation.newDelete(dirUri).build());
+		// Delete all items
+		cpo.add(ContentProviderOperation.newDelete(dirUri).build());
 
-        try {
-            JSONArray array = RemoteEndpointUtil.fetchJsonArray();
-            if (array == null) {
-                throw new JSONException("Invalid parsed item array" );
-            }
+		try {
+			JSONArray array = RemoteEndpointUtil.fetchJsonArray();
+			if (array == null) {
+				throw new JSONException("Invalid parsed item array");
+			}
 
-            for (int i = 0; i < array.length(); i++) {
-                ContentValues values = new ContentValues();
-                JSONObject object = array.getJSONObject(i);
-                values.put(ItemsContract.Items.SERVER_ID, object.getString("id" ));
-                values.put(ItemsContract.Items.AUTHOR, object.getString("author" ));
-                values.put(ItemsContract.Items.TITLE, object.getString("title" ));
-                values.put(ItemsContract.Items.BODY, object.getString("body" ));
-                values.put(ItemsContract.Items.THUMB_URL, object.getString("thumb" ));
-                values.put(ItemsContract.Items.PHOTO_URL, object.getString("photo" ));
-                values.put(ItemsContract.Items.ASPECT_RATIO, object.getString("aspect_ratio" ));
+			for (int i = 0; i < array.length(); i++) {
+				ContentValues values = new ContentValues();
+				JSONObject object = array.getJSONObject(i);
+				values.put(ItemsContract.Items.SERVER_ID, object.getString("id"));
+				values.put(ItemsContract.Items.AUTHOR, object.getString("author"));
+				values.put(ItemsContract.Items.TITLE, object.getString("title"));
+				values.put(ItemsContract.Items.BODY, object.getString("body"));
+				values.put(ItemsContract.Items.THUMB_URL, object.getString("thumb"));
+				values.put(ItemsContract.Items.PHOTO_URL, object.getString("photo"));
+				values.put(ItemsContract.Items.ASPECT_RATIO, object.getString("aspect_ratio"));
 //                time.parse3339(object.getString("published_date"));
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(object.getString("published_date")));
-//                values.put(ItemsContract.Items.PUBLISHED_DATE, time.toMillis(false));
-                cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
-            }
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(object.getString("published_date")));
+				values.put(ItemsContract.Items.PUBLISHED_DATE, cal.getTimeInMillis());
+//				values.put(ItemsContract.Items.PUBLISHED_DATE, time.toMillis(false));
+				cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
+			}
 
-            getContentResolver().applyBatch(ItemsContract.CONTENT_AUTHORITY, cpo);
+			getContentResolver().applyBatch(ItemsContract.CONTENT_AUTHORITY, cpo);
 
-        } catch (JSONException | RemoteException | OperationApplicationException | ParseException e) {
-            Log.e(TAG, "Error updating content.", e);
-        }
+		}
+		catch (JSONException | RemoteException | OperationApplicationException | ParseException e) {
+			Log.e(TAG, "Error updating content.", e);
+		}
 
-        sendStickyBroadcast(
-                new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, false));
-    }
+		sendStickyBroadcast(
+				new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, false));
+	}
 }
